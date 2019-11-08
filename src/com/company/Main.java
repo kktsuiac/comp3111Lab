825e8ff3888151d8338213836d69646da2ebcc76
tsuik <tsuikakit0605@icloud.com>
2019-11-08 10:48:20 +0800

    change in yankees

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
+        System.out.println("change in Yankees");
         System.out.println("change in RedSox");
     }
 }
