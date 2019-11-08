6c45af53a8a47e9af6214dd084c1543bc2f88470
tsuik <tsuikakit0605@icloud.com>
2019-11-08 11:15:18 +0800

    add gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        System.out.println("a change  in Yankees");
-        System.out.println("change in RedSox");
+        System.out.println("a change in Yankees");
+        System.out.println("change again in RedSox");
     }
 }
